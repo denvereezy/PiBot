9596e60cbaf941da14bf0bb2bbeee7ce8e09ee39
denver <denver@projectcodex.co>
2017-04-09 20:00:00 +0200

    front wheels code

---
+++ utils/front_wheels.js
@@ -0,0 +1,23 @@
+var five = require('johnny-five');
+
+module.exports = function(pwmPin, directPin, motorSpeed) {
+    const motor = new five.Motor({
+        pins: {
+            pwm: pwmPin,
+            dir: directPin
+        },
+        invertPWM: true
+    });
+
+    this.left = () => {
+        motorSpeed.speed === 0 ? motor.stop() : motor.reverse(motorSpeed.speed);
+    };
+
+    this.right = () => {
+        motorSpeed.speed === 0 ? motor.stop() : motor.forward(motorSpeed.speed);
+    };
+
+    this.stop = () => {
+        motor.stop();
+    };
+};
